9dc5ecfb796ffd298c204ba4838d2ff88c257fdc
Aaryan4701 <aaryan.aryalsharma@gmail.com>
2025-03-27 00:03:45 +1100

    Styling
    Styling

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path , include
+from django.contrib import admin
 from .views import TaskList , TaskDetail , TaskCreate , TaskUpdate , DeleteView , CustomLoginView , RegisterPage
 from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
+from django.contrib.staticfiles.views import serve
+
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -11,4 +15,6 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name= 'task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name= 'task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name= 'task-delete'),
+    path('sw.js', TemplateView.as_view(template_name="sw.js", content_type="application/javascript")),
+
 ] 
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    'pwa',
 ]
 
 MIDDLEWARE = [
@@ -117,10 +118,14 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
 
